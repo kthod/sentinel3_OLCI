315c573cec14cea72114f635055af8496def3051
kthod <73425244+kthod@users.noreply.github.com>
2023-10-10 22:35:06 +0300

    Add files via upload

--- download_SLSTR.py
+++ download_SLSTR.py
@@ -5,18 +5,18 @@ import os
 #orbit = sys.argv[1]
 api = SentinelAPI('thkalamarakis', 'th0doris6287', 'https://apihub.copernicus.eu/apihub')
 print(api)
-footprint = geojson_to_wkt(read_geojson(r"C:\Users\thodoris\Documents\Python_Scripts\s3\s3\map_CDN.geojson"))
+footprint = geojson_to_wkt(read_geojson('/home/thodorisk/Documents/Sentinel3_WV/sentinel3_OLCI/map_CDN.geojson'))
 print(footprint)
 products = api.query(footprint,
-                     producttype='SL_2_LST___',
-                     relativeorbitnumber= '78',
+                     producttype='SL_1_RBT___',
+                     #relativeorbitnumber= '7',
                      #cyclenumber = '052',
-                     #date=('20230425', date(2023, 7, 24)),
+                     date=('20191225', date(2020, 1, 24)),
                      platformname='Sentinel-3',
                      filename = "S3B_*",
                      instrumentshortname = "SLSTR",
                      #platformnumber = 'B'
-                     timeliness="Non Time Critical"
+                     #timeliness="Non Time Critical"
                      )
 #print(products)
 api.download_all(products)
